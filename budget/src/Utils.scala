c5abe83561a96eafafb059ceec60b4c6382576d4
Jacob Bennett <16541352+sc8ing@users.noreply.github.com>
2024-09-28 09:47:11 -0400

    local datetime json writer

--- budget/src/Utils.scala
+++ budget/src/Utils.scala
@@ -1,6 +1,16 @@
 package budget
 
+import java.time.LocalDateTime
 import zio.ZIO
 
 extension [R, E, A] (f: ZIO[R, E, A])
   def |[E2](e: E2): ZIO[R, E2, A] = f.orElseFail(e)
+
+object LocalDateTimeRW {
+  import upickle.default.*
+
+  implicit val rw: ReadWriter[LocalDateTime] = readwriter[String].bimap[LocalDateTime](
+    _.toString,
+    LocalDateTime.parse
+  )
+}
